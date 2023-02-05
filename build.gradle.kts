725660d59a8c14679fa662157acd2e4678ab569b
jubayar <jubayar.ict@gmail.com>
2023-02-05 08:38:32 +0800

    new app module

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,9 +11,7 @@ plugins {
     id("com.android.application") version "7.4.0" apply false
     id("com.android.library") version "7.4.0" apply false
     id("org.jetbrains.kotlin.android") version "1.7.20" apply false
-    kotlin("kapt") version "1.8.10" apply false
-
-
+    kotlin("kapt") version "1.7.20" apply false
 
     id("com.google.gms.google-services") version "4.3.8" apply false
     id("com.google.firebase.crashlytics") version "2.9.2" apply false
