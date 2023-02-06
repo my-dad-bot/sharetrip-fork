7c2f700165635001a2a6a5c229eb14e34380fa8b
jubayar <jubayar.ict@gmail.com>
2023-02-06 10:36:57 +0800

    Remove sharetrip app module

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,10 +12,6 @@ plugins {
     id("com.android.library") version "7.4.0" apply false
     id("org.jetbrains.kotlin.android") version "1.7.20" apply false
     kotlin("kapt") version "1.7.20" apply false
-
-    id("com.google.gms.google-services") version "4.3.8" apply false
-    id("com.google.firebase.crashlytics") version "2.9.2" apply false
-    id("com.google.firebase.firebase-perf") version "1.4.2" apply false
 }
 
 tasks.register("clean", Delete::class) {
